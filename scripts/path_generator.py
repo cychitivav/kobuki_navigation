427773ccf4908630c4de14b0a226f6695ec55e09
Cristian Chitiva <cychitivav@unal.edu.co>
2021-08-09 21:39:28 -0500

    path generator corrected

--- scripts/path_generator.py
+++ scripts/path_generator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import rospy
-import tf2_ros
+from tf.transformations import quaternion_from_euler
+import tf2_ros 
 import numpy as np
 from nav_msgs.msg import Path
 from geometry_msgs.msg import PoseStamped, TransformStamped, PointStamped
@@ -9,12 +10,15 @@ from tf_conversions import transformations as tf
 
 class generator:
     def __init__(self):
+        self.yaw_0 = rospy.get_param("yaw")
+        self.x_0 = rospy.get_param("x")
+        self.y_0 = rospy.get_param("y")
 
-	self.pub_trans()
+        self.set_initial_pose()
 
-	self.pub = rospy.Publisher('/path', Path, queue_size=10)
-	self.sub = rospy.Subscriber('/clicked_point', PointStamped, self.calc_path)
-    # self.sub = rospy.Subscriber('/move_base_simple/goal', PointStamped, self.calc_path)
+        self.pub = rospy.Publisher('/path', Path, queue_size=10)
+        self.sub = rospy.Subscriber('/clicked_point', PointStamped, self.calc_path)
+        # self.sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.calc_path)
 
     def calc_path(self, goal):   
         path = [[-3.5, 0.0],
@@ -31,34 +35,30 @@ class generator:
 
         while not rospy.is_shutdown():
             msg = Path()
-            msg.header.frame_id = "map"
+            msg.header.frame_id = "odom"
             msg.header.stamp = rospy.Time.now()
 
             for wp in path:
                 pose = PoseStamped()
 
-                pose.pose.position.x = wp[0]
-                pose.pose.position.y = wp[1]
+                pose.pose.position.x = wp[0]*np.cos(self.yaw_0) + wp[1]*np.sin(self.yaw_0)
+                pose.pose.position.y = - wp[0]*np.sin(self.yaw_0) + wp[1]*np.cos(self.yaw_0)
 
                 msg.poses.append(pose)
 
             self.pub.publish(msg)
 
+    
+    def set_initial_pose(self):
+        trans = TransformStamped()
+        trans.header.stamp = rospy.Time.now()
+        trans.header.frame_id = "map"
+        trans.child_frame_id = "odom"        
 
-    def pub_trans(self):
-        trans=TransformStamped()
-        trans.header.stamp=rospy.Time.now()
-        trans.header.frame_id= "map"
-        trans.child_frame_id="odom"
+        trans.transform.translation.x = self.x_0
+        trans.transform.translation.y = self.y_0
 
-        yaw_0 = rospy.get_param("yaw")
-        x_0 = rospy.get_param("x")
-        y_0 = rospy.get_param("y")
-
-        trans.transform.translation.x = x_0
-        trans.transform.translation.y = y_0
-
-        quat=tf.quaternion_from_euler(0.0,0.0,yaw_0)
+        quat=tf.quaternion_from_euler(0.0,0.0,self.yaw_0)
         trans.transform.rotation.x = quat[0]
         trans.transform.rotation.y = quat[1]
         trans.transform.rotation.z = quat[2]

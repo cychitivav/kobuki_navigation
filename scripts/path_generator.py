1a9a19ad8e3653a1931a3718f5c632fd91eea5f4
Cristian Chitiva <cychitivav@unal.edu.co>
2021-08-10 09:14:39 -0500

    Path generator wihtout bucle

--- scripts/path_generator.py
+++ scripts/path_generator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import rospy
-from tf.transformations import quaternion_from_euler
 import tf2_ros 
 import numpy as np
 from nav_msgs.msg import Path
@@ -18,9 +17,11 @@ class generator:
 
         self.pub = rospy.Publisher('/path', Path, queue_size=10)
         self.sub = rospy.Subscriber('/clicked_point', PointStamped, self.calc_path)
-        # self.sub = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.calc_path)
 
     def calc_path(self, goal):   
+        x_g = goal.point.x
+        y_g = goal.point.y
+
         path = [[-3.5, 0.0],
                 [-3.5, 3.5],
                 [1.5, 3.5],
@@ -33,20 +34,19 @@ class generator:
                 [1.5, -3.5],
                 [-1.0, -3.5]]
 
-        while not rospy.is_shutdown():
-            msg = Path()
-            msg.header.frame_id = "odom"
-            msg.header.stamp = rospy.Time.now()
+        msg = Path()
+        msg.header.frame_id = "odom"
+        msg.header.stamp = rospy.Time.now()
 
-            for wp in path:
-                pose = PoseStamped()
+        for wp in path:
+            pose = PoseStamped()
 
-                pose.pose.position.x = wp[0]*np.cos(self.yaw_0) + wp[1]*np.sin(self.yaw_0)
-                pose.pose.position.y = - wp[0]*np.sin(self.yaw_0) + wp[1]*np.cos(self.yaw_0)
+            pose.pose.position.x = wp[0]*np.cos(-self.yaw_0) - wp[1]*np.sin(-self.yaw_0)
+            pose.pose.position.y = wp[0]*np.sin(-self.yaw_0) + wp[1]*np.cos(-self.yaw_0)
 
-                msg.poses.append(pose)
+            msg.poses.append(pose)
 
-            self.pub.publish(msg)
+        self.pub.publish(msg)
 
     
     def set_initial_pose(self):
@@ -58,7 +58,7 @@ class generator:
         trans.transform.translation.x = self.x_0
         trans.transform.translation.y = self.y_0
 
-        quat=tf.quaternion_from_euler(0.0,0.0,self.yaw_0)
+        quat = tf.quaternion_from_euler(0.0,0.0,self.yaw_0)
         trans.transform.rotation.x = quat[0]
         trans.transform.rotation.y = quat[1]
         trans.transform.rotation.z = quat[2]

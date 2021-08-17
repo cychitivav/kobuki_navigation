b8a302375c60abead93d2ce2c4766a5f3bd7dd0f
Cristian Chitiva <cychitivav@unal.edu.co>
2021-08-17 16:35:42 -0500

    planning

--- scripts/path_generator.py
+++ scripts/path_generator.py
@@ -2,7 +2,7 @@
 import rospy
 import tf2_ros 
 import numpy as np
-from nav_msgs.msg import Path
+from nav_msgs.msg import Path, OccupancyGrid
 from geometry_msgs.msg import PoseStamped, TransformStamped, PointStamped
 from tf_conversions import transformations as tf
 import matplotlib.pyplot as plt
@@ -11,24 +11,21 @@ import cv2
 
 class generator:
     def __init__(self):
-        self.pub = rospy.Publisher('/plan', Path, queue_size=10)
-        self.sub = rospy.Subscriber('/clicked_point', PointStamped, self.calc_path)
-
-    def calc_path(self, goal): 
-        image = cv2.imread('map/map.pgm', 0)
-        _, umbral = cv2.threshold(image, 245, 255, cv2.THRESH_BINARY)        
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_OPEN, (3,3)) 
-        apertura = cv2.morphologyEx(umbral, cv2.MORPH_OPEN, kernel)
-
-        skel = cv2.ximgproc.thinning(apertura)
+        self.mapa = np.array([])
+        self.skel = np.array([])
         
-        # cv2.imshow("Original", image)
-        # cv2.imshow("Umbralizada", umbral)
-        # cv2.imshow("Umbralizada filtrada", apertura)
-        # cv2.imshow("Esqueleto", skel)
-        # cv2.waitKey(0) 
-
+        self.pub = rospy.Publisher('/plan', Path, queue_size=10)
+        
+        self.sub = rospy.Subscriber('/clicked_point', PointStamped, self.calc_path)
+        
+        self.sub_map = rospy.Subscriber('/map', OccupancyGrid, self.get_map)
+        self.pub_costmap = rospy.Publisher('/costmap', OccupancyGrid, queue_size=10)
+        
+        
+        
+        
+        
+    def calc_path(self, goal): 
         pose = PoseStamped()
         pose.pose.position.x = goal.point.x
         pose.pose.position.y = goal.point.y 
@@ -40,30 +37,92 @@ class generator:
 
         self.pub.publish(msg)
 
-    def voronoi(self,points,shape=(500,500)):
-        depthmap = np.ones(shape,np.float)*1e308
-        colormap = np.zeros(shape,np.int)
+    def rotate_image(self, image, angle):
+        image_center = tuple(np.array(image.shape[0:2]) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1)
 
-        for i,(x,y) in enumerate(points):
-            hypot = lambda (X,Y): (X-x)**2 + (Y-y)**2
+        vertical = cv2.warpAffine(image, rot_mat, image.shape[0:2], flags=cv2.INTER_CUBIC)
+        im = vertical.copy()
 
-            paraboloid = np.fromfunction(hypot,shape)
-            colormap = np.where(paraboloid < depthmap,i+1,colormap)
-            depthmap = np.where(paraboloid < depthmap,paraboloid,depthmap)
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                if i < 100 or j < 100 or j > 924 or i > 924:
+                    im[i,j] = 205
+                else:                    
+                    neighbor = 0
+                    if vertical[i+1,j] < 43.0:
+                        neighbor += 1
+                    if vertical[i-1,j] < 43.0:
+                        neighbor += 1
+                    if vertical[i+1,j-1] < 43.0:
+                        neighbor += 1
+                    if vertical[i+1,j+1] < 43.0:
+                        neighbor += 1
+                    if vertical[i-1,j+1] < 43.0:
+                        neighbor += 1
+                    if vertical[i-1,j-1] < 43.0:
+                        neighbor += 1
+                    if vertical[i,j+1] < 43.0:
+                        neighbor += 1
+                    if vertical[i,j-1] < 43.0:
+                        neighbor += 1
+                        
+                    if neighbor >= 5:
+                        im[i,j] = 0.0          
+        return im
+    
+    def get_skeleton(self, image):
+        _, th = cv2.threshold(image, 245, 255, cv2.THRESH_BINARY)        
 
-        for (x,y) in points:
-            colormap[x-1:x+2,y-1:y+2] = 0.0
+        kernel = cv2.getStructuringElement(cv2.MORPH_OPEN, (3,3)) 
+        op = cv2.morphologyEx(th, cv2.MORPH_OPEN, kernel)
 
-        return colormap
+        skel = cv2.ximgproc.thinning(op)
+    
+        return skel
+    
+    def get_map(self, grid_map):
+        mapa = []
+        row = []
+        
+        for i in range(len(grid_map.data)):
+            if grid_map.data[i] == -1:
+                row.append(20)
+            else:
+                row.append(grid_map.data[i])
+        
+            if (i+1) % grid_map.info.width == 0:
+                mapa.append(row)
+                row = []
+                
+        mapa = np.array(mapa)/100.0*-255.0+255.0
+        self.mapa = mapa.astype(np.uint8)
+        
+        rotated = self.mapa.copy()#rotate_image(self.mapa, -7.66)
+        self.skel = self.get_skeleton(rotated)
+        
+        alpha = 0.3
+        costmap = self.skel.copy()
+        for i in range(1,4):
+            blur = cv2.GaussianBlur(costmap,(10*i-1,10*i-1),20)
+            _, th = cv2.threshold(blur, 2, 255, cv2.THRESH_BINARY)        
+            costmap = cv2.addWeighted(th, alpha, costmap, 1-alpha, 0.0)
+        
+        alpha = 0.6
+        _, th = cv2.threshold(rotated, 245, 255, cv2.THRESH_BINARY)
+        
+        costmap = cv2.addWeighted(th, 1-alpha, costmap, alpha, 0.0)
+        costmap = costmap /255.0*100.0
+        
+        cm = OccupancyGrid()
+        cm.header = grid_map.header       
+        cm.info = grid_map.info
+        
+        cm.data = costmap.flatten().astype(np.uint8)
+        
+        
+        self.pub_costmap.publish(cm)
 
-    def rotate_image(self)
-        img = cv2.imread('maps/map.pgm')
-        rotated = ndimage.rotate(img, -8) # 8 deg
-        nimg = cv2.cvtColor(rotated, cv2.COLOR_RGB2GRAY)
-
-        cv2.imwrite('map/rotated.pgm',nimg)
-
-        return nimg
 
 if __name__ == '__main__':
     rospy.init_node('path_gen', anonymous=True)
--- test/paths.py
+++ test/paths.py
@@ -1,16 +1,108 @@
+#!/usr/bin/python
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
-image = cv2.imread('map/map.pgm', 0)
-_, umbral = cv2.threshold(image, 245, 255, cv2.THRESH_BINARY)        
 
-kernel = cv2.getStructuringElement(cv2.MORPH_OPEN, (3,3)) 
-apertura = cv2.morphologyEx(umbral, cv2.MORPH_OPEN, kernel)
+def get_skeleton(image):
+    _, th = cv2.threshold(image, 245, 255, cv2.THRESH_BINARY)        
 
-skel = cv2.ximgproc.thinning(apertura)
+    kernel = cv2.getStructuringElement(cv2.MORPH_OPEN, (3,3)) 
+    op = cv2.morphologyEx(th, cv2.MORPH_OPEN, kernel)
 
-cv2.imshow("Original", image)
-cv2.imshow("Umbralizada", umbral)
-cv2.imshow("Umbralizada filtrada", apertura)
-cv2.imshow("Esqueleto", skel)
-cv2.waitKey(0) 
+    skel = cv2.ximgproc.thinning(op)
+    
+    return skel
+
+
+def rotate_image(image, angle):
+    image_center = tuple(np.array(image.shape[0:2]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1)
+
+    vertical = cv2.warpAffine(image, rot_mat, image.shape[0:2], flags=cv2.INTER_CUBIC)
+    im = vertical.copy()
+
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if i < 100 or j < 100 or j > 924 or i > 924:
+                im[i,j] = 205
+            else:                    
+                neighbor = 0
+                if vertical[i+1,j] < 43.0:
+                    neighbor += 1
+                if vertical[i-1,j] < 43.0:
+                    neighbor += 1
+                if vertical[i+1,j-1] < 43.0:
+                    neighbor += 1
+                if vertical[i+1,j+1] < 43.0:
+                    neighbor += 1
+                if vertical[i-1,j+1] < 43.0:
+                    neighbor += 1
+                if vertical[i-1,j-1] < 43.0:
+                    neighbor += 1
+                if vertical[i,j+1] < 43.0:
+                    neighbor += 1
+                if vertical[i,j-1] < 43.0:
+                    neighbor += 1
+                    
+                if neighbor >= 5:
+                    im[i,j] = 0.0  
+    
+    return im
+
+    
+
+if __name__ == "__main__":
+    
+    
+    image = cv2.imread('map/map.pgm', 0)
+    rotated = rotate_image(image, -7.66)
+    skel = get_skeleton(rotated)
+    
+    alpha = 0.3
+    costmap = skel.copy()
+    for i in range(1,4):
+        blur = cv2.GaussianBlur(costmap,(10*i-1,10*i-1),20)
+        _, th = cv2.threshold(blur, 2, 255, cv2.THRESH_BINARY)        
+        costmap = cv2.addWeighted(th, alpha, costmap, 1-alpha, 0.0)
+    
+    alpha = 0.6
+    _, th = cv2.threshold(rotated, 245, 255, cv2.THRESH_BINARY)
+    costmap = cv2.addWeighted(th, 1-alpha, costmap, alpha, 0.0)
+    
+    x_0, y_0 = [500,508]
+    
+    x_f = np.random.randint(487) + 270
+    y_f = np.random.randint(448) + 363
+    
+    points = []
+    path = cv2.cvtColor(skel,cv2.COLOR_GRAY2RGB)
+    # path[x_0,y_0,:] = [0,0,255]
+    # path[x_f,y_f,:] = [0,255,0]
+    
+    # dst = cv2.cornerHarris(skel,2,3,0.04)
+    # dst = cv2.dilate(dst,None)
+    # ret, dst = cv2.threshold(dst,0.01*dst.max(),255,0)
+    # dst = np.uint8(dst)
+    # # find centroids
+    # ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+    
+    dst = cv2.cornerHarris(skel,2,5,0.04)
+    # dst = np.uint8(dst)
+    # ret, labels, stats, centroids = cv2.connectedComponentsWithStats(dst)
+    # print centroids
+    
+    # kernel = cv2.getStructuringElement(cv2.MORPH_ERODE, (3,3))
+    # #dst = cv2.erode(dst, kernel)
+    
+    path[dst>0.0*dst.max()]=[0,0,255]
+
+    # cv2.imshow("Original", image)
+    # cv2.imshow("Rotada -7.66 grados", rotated)
+    # cv2.imshow("Esqueleto", skel)
+    # cv2.imshow("costmap", costmap)
+    cv2.imshow("path", path)
+    
+    # cv2.imwrite('map/rotated.pgm', rotated)
+    cv2.waitKey(0) 
+    
